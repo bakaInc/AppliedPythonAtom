f53405b6621493210a7fd3392dc1560be1157afd
Rustam <rustamelutingol@gmail.com>
2019-03-05 18:45:09 +0300

    done det

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -11,4 +11,21 @@ def calculate_determinant(list_of_lists):
     :param list_of_lists: список списков - исходная матрица
     :return: значение определителя или None
     '''
-    raise NotImplementedError
+    dim = len(list_of_lists)
+    det = 0
+    sign = 1
+    i = 0
+    for j in range(dim):
+        if len(list_of_lists[i]) != dim:
+            return None
+        minors = []
+        for k in range(dim - 1): 
+            minors.append([])
+            for m in range(dim): 
+                if m != j:
+                    minors[k].append(list_of_lists[k + 1][m])
+
+        det += sign * list_of_lists[i][j] * (calculate_determinant(minors) or 1)
+        sign = -sign
+
+    return det
