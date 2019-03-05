916451b15166dd72099bfbe86231075ef88dea7b
Rustam <rustamelutingol@gmail.com>
2019-03-05 19:57:24 +0300

    pep8

--- homeworks/homework_01/hw1_det.py
+++ homeworks/homework_01/hw1_det.py
@@ -19,13 +19,14 @@ def calculate_determinant(list_of_lists):
         if len(list_of_lists[i]) != dim:
             return None
         minors = []
-        for k in range(dim - 1): 
+        for k in range(dim - 1):
             minors.append([])
-            for m in range(dim): 
+            for m in range(dim):
                 if m != j:
                     minors[k].append(list_of_lists[k + 1][m])
 
-        det += sign * list_of_lists[i][j] * (calculate_determinant(minors) or 1)
+        det += sign * list_of_lists[i][j] * \
+                    (calculate_determinant(minors) or 1)
         sign = -sign
 
     return det
