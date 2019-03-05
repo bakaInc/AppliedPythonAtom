d22f9f24fb6bcd04d9cc3a5eebe205e2b564be3a
Rustam <rustamelutingol@gmail.com>
2019-03-05 18:34:42 +0300

    Done invertdict

--- homeworks/homework_01/hw1_invertdict.py
+++ homeworks/homework_01/hw1_invertdict.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
+def invert(value):
+    """
+    Функция разворачивающая вложенные
+    друг в друга словари в один
+    :param value: list, tuple or set
+    :return: result_list: list
+    """
+    result_list = []
+    if isinstance(value, list) or isinstance(value, set):
+        for i in value:
+            result_list += invert(i)
+    else:
+        result_list.append(value)
+    return result_list
+    
 def invert_dict(source_dict):
     '''
     Функция которая разворачивает словарь, т.е.
@@ -9,4 +23,17 @@ def invert_dict(source_dict):
     :param source_dict: dict
     :return: new_dict: dict
     '''
-    raise NotImplementedError
+    if not isinstance(source_dict, dict):
+        return None
+    new_dict = {}
+    for key, value in source_dict.items():
+        for i in invert(value):
+            if i in new_dict:
+                if isinstance(new_dict[i], list):
+                    new_dict[i].append(key)
+                else:
+                    item = new_dict.get(i)
+                    new_dict[i] = [item, key]
+            else:
+                new_dict[i] = key
+    return new_dict
