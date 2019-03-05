1d63eec1656fdd781a81a6eb6e45dc1dedb1f512
Rustam <rustamelutingol@gmail.com>
2019-03-05 20:16:11 +0300

    Done simple calc

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -10,4 +10,17 @@ def calculator(x, y, operator):
     :param operator: 4 оператора: plus, minus, mult, divide
     :return: результат операции или None, если операция не выполнима
     '''
-    raise NotImplementedError
+    try:
+        x, y = float(x), float(y)
+    except (ValueError, TypeError):
+        return None
+    if operator == "plus":
+        return x + y
+    elif operator == "minus":
+        return x - y
+    elif operator == "mult":
+        return x * y
+    elif operator == "divide":
+        return x / y if y != 0 else None
+    else:
+        return None
