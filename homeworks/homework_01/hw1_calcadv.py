bbacbe732e8cda9b50dbc2f886e2e4b5193fb07c
bakaInc <sidorovivan01998@gmail.com>
2019-03-07 01:53:10 +0300

    Update hw1_calcadv.py

--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -1,15 +1,58 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+'''
+Калькулятор на основе обратной польской записи.
+Разрешенные операции: открытая скобка, закрытая скобка,
+плюс, минус, умножить, делить
+:param input_string: строка, содержащая выражение
+:return: результат выполнение операции, если строка валидная - иначе None
+'''
+
+def is_bracket_correct(input_string):
+    stack = []
+    for elem in input_string:
+        if elem not in set('({[)}]'):
+            continue
+        if elem in set('({['):
+            stack.append(elem)
+            continue
+        if len(stack) == 0:
+            return False
+        openingBracket = stack.pop()
+        if (not (
+                (openingBracket == '(' and elem == ')') or
+                (openingBracket == '[' and elem == ']') or
+                (openingBracket == '{' and elem == '}')
+                )):
+            return False
+    if len(stack) != 0:
+        return False
+    return True
+
+def splitArr(arr):
+    arr = arr.split()
+    delimeters = ['(', ')', '+', '-', '*', '/']
+
+    newArr = []
+
+    for tmp in arr:
+        res = ""
+        for ch in tmp:
+            if ch.isdigit() or ch.isalpha():
+                res += ch
+            elif ch in delimeters:
+                newArr.append(res)
+                newArr.append(ch)
+                res = ""
+        newArr.append(res)
+    newArr = [value for value in newArr if value != '']
+    return newArr
+
 
 def advanced_calculator(input_string):
-    '''
-    Калькулятор на основе обратной польской записи.
-    Разрешенные операции: открытая скобка, закрытая скобка,
-     плюс, минус, умножить, делить
-    :param input_string: строка, содержащая выражение
-    :return: результат выполнение операции, если строка валидная - иначе None
-    '''
+    if not is_bracket_correct(input_string):
+        return None
     for i in range(10):
         input_string = input_string.replace(str(i) + " ", str(i) + "|")
     while " " in input_string:
@@ -22,99 +65,76 @@ def advanced_calculator(input_string):
         input_string = input_string.replace("++", "+")
     while "+-" in input_string:
         input_string = input_string.replace("+-", "-")
+    while "**" in input_string:
+        return None
+    while "//" in input_string:
+        return None
     while "(-" in input_string:
         input_string = input_string.replace("(-", "(0-")
     while "/-" in input_string:
         input_string = input_string.replace("/-", "*(0-1)/")
     while "*-" in input_string:
         input_string = input_string.replace("*-", "*(0-1)*")
-    if len(input_string) > 0 and (input_string[0] is
-                                  "-" or input_string[0] is "+"):
-        input_string = "0" + input_string
-    elem = ""
-    output_list = []
-    stack = []
+    while "*+" in input_string:
+        input_string = input_string.replace("*+", "*(0+1)*")
 
-    def is_op(operator):
-        return (operator is "+" or operator is "-" or
-                operator is "/" or operator is "*")
+    if len(input_string) > 0:
+        if input_string[0] is "-" or input_string[0] is "+":
+            input_string = "0" + input_string
+        if input_string[0] is "*" or input_string[0] is "/":
+            return None
+    print("input string ", input_string)
+    res = []
+    opers = []
+    operators = {'+': 2, '-': 2, '*': 1, '/': 1}
 
-    while len(input_string) > 0:
-        if is_op(input_string[0]):
-            if len(elem) > 0:
-                try:
-                    output_list.append(float(elem))
-                    elem = ""
-                except (TypeError, ValueError):
-                    return None
-            while len(stack) > 0 and stack[len(stack) - 1] is not "(":
-                if stack[len(stack) - 1] is '*' or input_string[0] is '+' \
-                   or stack[len(stack) - 1] is '/' or input_string[0] is '-':
-                    output_list.append(stack.pop())
-                else:
-                    break
-            stack.append(input_string[0])
-            input_string = input_string[1:]
-        elif input_string[0] is "(":
-            if len(elem) > 0:
-                return None
-            stack.append("(")
-            input_string = input_string[1:]
-        elif input_string[0] is ")":
-            if len(elem) == 0:
-                return None
-            try:
-                output_list.append(float(elem))
-                elem = ""
-            except (TypeError, ValueError):
-                return None
-            input_string = input_string[1:]
-            try:
-                elem = stack.pop()
-                while elem is not "(":
-                    output_list.append(elem)
-                    elem = stack.pop()
-                elem = ""
-            except IndexError:
-                return None
-        elif input_string[0].isdigit() or input_string[0] is ".":
-            elem += input_string[0]
-            input_string = input_string[1:]
-        elif input_string[0] is "|":
-            output_list.append(float(elem))
-            elem = ""
-            input_string = input_string[1:]
-        else:
-            return None
-    if len(elem) > 0:
-        try:
-            output_list.append(float(elem))
-            elem = ""
-        except (ValueError, TypeError):
-            return None
-    while len(stack) > 0:
-        output_list.append(stack.pop())
+    input_string = splitArr(input_string)
+
     try:
-        while len(output_list) > 0:
-            elem = output_list.pop(0)
-            if isinstance(elem, float):
-                stack.append(elem)
+        for ch in input_string:
+            if ch.isalpha():
+                return None
+            if ch.isdigit():
+                res.append(ch)
+            elif ch == '(':
+                opers.append(ch)
+            elif ch == ')':
+                while opers[-1] != '(':
+                    res.append(opers.pop())
+                opers.pop()
+            elif opers and opers[-1] != '(':
+                print(opers)
+                if operators.get(opers[-1]) <= operators.get(ch):
+                    res.append(opers.pop())
+                    opers.append(ch)
+                else:
+                    opers.append(ch)
             else:
-                elem2 = stack.pop()
-                elem1 = stack.pop()
-                if elem is "+":
-                    stack.append(elem1 + elem2)
-                elif elem is "-":
-                    stack.append(elem1 - elem2)
-                elif elem is "/":
-                    try:
-                        stack.append(elem1 / elem2)
-                    except ZeroDivisionError:
-                        return None
-                elif elem is "*":
-                    stack.append(elem1 * elem2)
-    except IndexError:
+                opers.append(ch)
+        if len(res) < 1:
+            return None
+        while opers:
+            res.append(opers.pop())
+
+        for el in res:
+            if el.isdigit():
+                opers.append(el)
+            else:
+                d2 = int(opers.pop())
+                d1 = int(opers.pop())
+
+                if el == '+':
+                    opers.append(d1 + d2)
+                if el == '-':
+                    opers.append(d1 - d2)
+                if el == '*':
+                    opers.append(d1 * d2)
+                if el == '/':
+                    opers.append(d1 / d2)
+    except:
         return None
-    if len(stack) != 1:
+    if len(opers) == 1:
+        return int(opers[0])
+    else:
         return None
-    return stack[0]
+advanced_calculator("-284.542933687587 / -96.8210086008016 265.0102127876513 * 21.626110896516877 ё -284.542933687587 / г")
