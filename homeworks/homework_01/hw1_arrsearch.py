7fca0ea13589f30999b619ed6488b38494b0b69c
bakaInc <sidorovivan01998@gmail.com>
2019-03-07 00:16:49 +0300

    Update hw1_arrsearch.py

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -2,28 +2,30 @@
 # coding: utf-8
 
 '''
-Метод возвращает индексы двух различных элементов listа
-, таких, что сумма этих элементов равна n.
-В случае, если таких элементов в массиве нет, то возвращается None
-Ограничение по времени O(n)
-:param input_list: список произвольной длины целых чисел
-:param n: целевая сумма
-:return: tuple из двух индексов или None
+Метод проверяющий является ли поданная скобочная
+последовательность правильной (скобки открываются и закрываются)
+не пересекаются
+:param input_string: строка, содержащая 6 типов скобок (,),[,],{,}
+:return: True or False
 '''
 
-def find_indices(arr, x):
-    l = len(arr)
-    left = 0
-    right = l - 1
-    arr.sort()
-
-    while (left != right):
-        sum = arr[left] + arr[right]
-        if sum < x:
-            left += 1
-        elif sum > x:
-            right -= 1
-        else:
-            return (left, right)
-
-    return None
+def is_bracket_correct(input_string):
+    stack = []
+    for elem in input_string:
+        if len(stack) == 0:
+            return False
+        if elem not in set('({[)}]'):
+            continue
+        if elem in set('({['):
+            stack.append(elem)
+            continue
+        openingBracket = stack.pop()
+        if (not (
+                (openingBracket == '(' and elem == ')') or
+                (openingBracket == '[' and elem == ']') or
+                (openingBracket == '{' and elem == '}')
+                )):
+            return False
+    if len(stack) != 0:
+        return False
+    return True
