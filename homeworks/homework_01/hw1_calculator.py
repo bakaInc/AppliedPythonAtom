3e4c7c9c3380f0f37d600f770944ae75723fd997
bakaInc <sidorovivan01998@gmail.com>
2019-03-07 00:42:58 +0300

    Update hw1_calculator.py

--- homeworks/homework_01/hw1_calculator.py
+++ homeworks/homework_01/hw1_calculator.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 '''
 Простенький калькулятор в прямом смысле. Работает c числами
 :param x: первый агрумент
@@ -8,15 +7,17 @@
 :param operator: 4 оператора: plus, minus, mult, divide
 :return: результат операции или None, если операция не выполнима
 '''
+
+
 def calculator(x, y, operator):
     try:
         int(x)
         int(y)
         result = {
-            'plus': lambda x, y   : (x + y),
-            'minus': lambda x, y  : (x - y),
-            'mult': lambda x, y   : float(x * y),
-            'divide': lambda x, y : (x / y),
+            'plus': lambda x, y: (x + y),
+            'minus': lambda x, y: (x - y),
+            'mult': lambda x, y: float(x * y),
+            'divide': lambda x, y: (x / y),
         }.get(operator, None)
         return result(x, y)
     except:
