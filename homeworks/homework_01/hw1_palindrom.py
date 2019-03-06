f21e47a1dbfcadbcf66b1cc345d0883eae3c4620
bakaInc <sidorovivan01998@gmail.com>
2019-03-06 23:25:47 +0300

    Update hw1_palindrom.py

--- homeworks/homework_01/hw1_palindrom.py
+++ homeworks/homework_01/hw1_palindrom.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+'''
+Метод проверяющий строку на то, является ли она палиндромом.
+:param input_string: строка
+:return: True, если строка являестя палиндромом
+False иначе
+'''
 
-def check_palindrom(input_string):
-    '''
-    Метод проверяющий строку на то, является ли
-    она палиндромом.
-    :param input_string: строка
-    :return: True, если строка являестя палиндромом
-    False иначе
-    '''
-    raise NotImplementedError
+def invertInt(x):
+    return (int)(str(x)[::-1])
+
+def check_palindrom(x):
+    if x == invertInt(x):
+        return True
+    else:
+        return False
