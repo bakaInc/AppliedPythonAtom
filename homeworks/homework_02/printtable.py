8e81db40975ec33386050eef46a7c02752e0de1c
bakaInc <sidorovivan01998@gmail.com>
2019-03-13 22:49:35 +0300

    Update printtable.py

--- homeworks/homework_02/printtable.py
+++ homeworks/homework_02/printtable.py
@@ -1,25 +1,24 @@
 
 def print_table(data):
-    maxCol=[]
+    maxCol = []
     for i in range(len(data[0])):
         x = 0
         for j in range(len(data)):
             x = max(x, len(str(data[j][i])))
         maxCol.append(x)
-    
     length = sum(maxCol)
-    
+
     strk = (('-' * (6 * len(maxCol)) + '-' * (length-3)))
     print(strk)
-	
-    mk = len(data[0])-1
-  
+	mk = len(data[0])-1
     for i in range(0, len(data)):
         print('|', end='')
         for j in range(len(data[i])):
             if j == (len(data[i]) - 1):
-                print(" " * (maxCol[mk] - len(str(data[i][j])) + 1), data[i][j], " |", end='')
+                print(" " * (maxCol[mk] - len(str(data[i][j])) + 1),
+		      data[i][j], " |", end='')
             else:
-                print(" ", data[i][j], ' ' * (maxCol[j] - len(str(data[i][j]))), '|', end='')
+                print(" ", data[i][j],
+		      ' ' * (maxCol[j] - len(str(data[i][j]))), '|', end='')
         print()
     print('-' * (sum(maxCol) - 3) + '-' * len(maxCol) * 6)
