d92111483a6656e0436152aecbbcf1f126123c29
bakaInc <sidorovivan01998@gmail.com>
2019-03-13 22:50:53 +0300

    Update table.py

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,7 +1,7 @@
 import sys
 
-from get_parsed import get_csv,get_json,check_format
-from encoding import get_c1251,get_utf_8,get_utf_16,get_encodeFile
+from get_parsed import get_csv, get_json,check_format
+from encoding import get_c1251, get_utf_8, get_utf_16, get_encodeFile
 from printtable import print_table
 
 if __name__ == '__main__':
