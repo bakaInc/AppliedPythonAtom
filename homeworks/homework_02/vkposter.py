38d03b271bd35a15f2954a1b18342cb1ddd9bc39
bakaInc <sidorovivan01998@gmail.com>
2019-03-14 08:08:45 +0300

    Update vkposter.py

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -9,55 +9,40 @@ from homeworks.homework_02.fastmerger import FastSortedListMerger
 class VKPoster:
 
     def __init__(self):
-        raise NotImplementedError
+        self.user_post = {}
+        self.user_follow = {}
+        self.posted_posts = {}
 
-    def user_posted_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        выложил пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        def user_posted_post(self, user_id: int, post_id: int):
+            if user_id in self.user_post.keys():
+                self.user_post[user_id].append(post_id)
+            else:
+                self.user_post[user_id] = [post_id]
 
-    def user_read_post(self, user_id: int, post_id: int):
-        '''
-        Метод который вызывается когда пользователь user_id
-        прочитал пост post_id.
-        :param user_id: id пользователя. Число.
-        :param post_id: id поста. Число.
-        :return: ничего
-        '''
-        pass
+        def user_read_post(self, user_id: int, post_id: int):
+            if post_id in self.posted_posts.keys():
+                self.posted_posts[post_id].add(user_id)
+            else:
+                self.posted_posts[post_id] = ["", [user_id]]
 
-    def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-        '''
-        Метод который вызывается когда пользователь follower_user_id
-        подписался на пользователя followee_user_id.
-        :param follower_user_id: id пользователя. Число.
-        :param followee_user_id: id пользователя. Число.
-        :return: ничего
-        '''
-        pass
+        def user_follow_for(self, follower_user_id: int, followee_user_id: int):
+            if follower_user_id in self.user_follow.keys():
+                self.user_follow.get(follower_user_id).append(followee_user_id)
+            else:
+                self.user_follow[follower_user_id] = [followee_user_id]
 
-    def get_recent_posts(self, user_id: int, k: int)-> list:
-        '''
-        Метод который вызывается когда пользователь user_id
-        запрашивает k свежих постов людей на которых он подписан.
-        :param user_id: id пользователя. Число.
-        :param k: Сколько самых свежих постов необходимо вывести. Число.
-        :return: Список из post_id размером К из свежих постов в
-        ленте пользователя. list
-        '''
-        pass
+        def get_recent_posts(self, user_id: int, k: int) -> list:
+            recent_post = []
+            for user in self.user_follow[user_id]:
+                if user in self.user_post.keys():
+                    for i in self.user_post[user]:
+                        recent_post.append(i)
+            recent_post.sort(reverse=True)
+            return recent_post[:k]
 
-    def get_most_popular_posts(self, k: int) -> list:
-        '''
-        Метод который возвращает список k самых популярных постов за все время,
-        остортированных по свежести.
-        :param k: Сколько самых свежих популярных постов
-        необходимо вывести. Число.
-        :return: Список из post_id размером К из популярных постов. list
-        '''
-        pass
+        def get_most_popular_posts(self, k: int) -> list:
+            sorted_posts = list(self.posts.keys())
+            sorted_posts.sort(
+                key=lambda input: (len(self.posted_posts.get(input)[1]),
+                                   input), reverse=True)
+            return sorted_posts[:k:]
