ee15152e73c6d2c043ef667b6b0f7ee2cf9e70d6
bakaInc <sidorovivan01998@gmail.com>
2019-03-14 07:26:20 +0300

    Update table.py

--- homeworks/homework_02/table.py
+++ homeworks/homework_02/table.py
@@ -1,6 +1,6 @@
 import sys
 
-from get_parsed import get_csv, get_json,check_format
+from get_parsed import get_csv, get_json, check_format
 from encoding import get_c1251, get_utf_8, get_utf_16, get_encodeFile
 from printtable import print_table
 
@@ -8,4 +8,3 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     data = check_format(get_encodeFile(filename))
     print_table(data)
-
