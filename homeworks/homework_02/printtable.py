ce08f11763df9cc4a56a3abcd1efe09cc3857fb5
bakaInc <sidorovivan01998@gmail.com>
2019-03-14 07:22:23 +0300

    Update printtable.py

--- homeworks/homework_02/printtable.py
+++ homeworks/homework_02/printtable.py
@@ -1,4 +1,5 @@
 
+
 def print_table(data):
     maxCol = []
     for i in range(len(data[0])):
@@ -7,18 +8,18 @@ def print_table(data):
             x = max(x, len(str(data[j][i])))
         maxCol.append(x)
     length = sum(maxCol)
-
-    strk = (('-' * (6 * len(maxCol)) + '-' * (length-3)))
+    strk = '-' * (6 * len(maxCol)) + '-' * (length-3)
     print(strk)
-	mk = len(data[0])-1
+    mk = len(data[0])-1
     for i in range(0, len(data)):
         print('|', end='')
         for j in range(len(data[i])):
             if j == (len(data[i]) - 1):
-                print(" " * (maxCol[mk] - len(str(data[i][j])) + 1),
-		      data[i][j], " |", end='')
+                print(" " * (maxCol[mk] - len(str(data[i][j])) + 1),end='')
+                print(data[i][j], " |", end='')
             else:
-                print(" ", data[i][j],
-		      ' ' * (maxCol[j] - len(str(data[i][j]))), '|', end='')
+                print(" ", data[i][j], end='')
+                print(' ' * (maxCol[j] - len(str(data[i][j]))), end='')
+                print('|', end='')
         print()
     print('-' * (sum(maxCol) - 3) + '-' * len(maxCol) * 6)
