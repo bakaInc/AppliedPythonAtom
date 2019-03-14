3f759a776c0d63ceb3492a420c5a3d4557d72e97
bakaInc <sidorovivan01998@gmail.com>
2019-03-14 08:16:19 +0300

    pip-pep

--- homeworks/homework_02/vkposter.py
+++ homeworks/homework_02/vkposter.py
@@ -9,41 +9,53 @@ from homeworks.homework_02.fastmerger import FastSortedListMerger
 class VKPoster:
 
     def __init__(self):
-        self.user_post = {}
+        self.autor_post = {}
+        self.post_reader = {}
         self.user_follow = {}
-        self.posted_posts = {}
 
-        def user_posted_post(self, user_id: int, post_id: int):
-            if user_id in self.user_post.keys():
-                self.user_post[user_id].append(post_id)
-            else:
-                self.user_post[user_id] = [post_id]
+    def user_posted_post(self, user_id: int, post_id: int):
+        if user_id in self.autor_post.keys():
+            self.autor_post[user_id].append(post_id)
+        else:
+            self.autor_post[user_id] = [post_id]
 
-        def user_read_post(self, user_id: int, post_id: int):
-            if post_id in self.posted_posts.keys():
-                self.posted_posts[post_id].add(user_id)
-            else:
-                self.posted_posts[post_id] = ["", [user_id]]
+    def user_read_post(self, user_id: int, post_id: int):
+        if post_id in self.post_reader.keys():
+            self.post_reader[post_id].add(user_id)
+        else:
+            self.post_reader[post_id] = set([user_id])
 
-        def user_follow_for(self, follower_user_id: int, followee_user_id: int):
-            if follower_user_id in self.user_follow.keys():
-                self.user_follow.get(follower_user_id).append(followee_user_id)
-            else:
-                self.user_follow[follower_user_id] = [followee_user_id]
+    def user_follow_for(self, follower_user_id: int, followee_user_id: int):
+        if follower_user_id in self.user_follow.keys():
+            self.user_follow[follower_user_id].append(followee_user_id)
+        else:
+            self.user_follow[follower_user_id] = [followee_user_id]
 
-        def get_recent_posts(self, user_id: int, k: int) -> list:
-            recent_post = []
-            for user in self.user_follow[user_id]:
-                if user in self.user_post.keys():
-                    for i in self.user_post[user]:
-                        recent_post.append(i)
-            recent_post.sort(reverse=True)
-            return recent_post[:k]
+    def get_recent_posts(self, user_id: int, k: int) -> list:
+        out = []
+        for user in self.user_follow[user_id]:
+            if user in self.autor_post.keys():
+                for i in self.autor_post[user]:
+                    out.append(i)
+        return sorted(out, reverse=True)[:k]
 
-        def get_most_popular_posts(self, k: int) -> list:
-            sorted_posts = list(self.posts.keys())
-            sorted_posts.sort(
-                key=lambda input:
-                (len(self.posted_posts.get(input)[1]), input),
-                reverse=True)
-            return sorted_posts[:k:]
+    def get_most_popular_posts(self, k: int) -> list:
+        out_num = []
+        out = []
+        N = 0
+        for post in self.post_reader.keys():
+            out_num.append(len(self.post_reader[post]))
+        out_num_s = sorted(out_num, reverse=True)
+        i = -1
+        while i < len(out_num):
+            i += 1
+            out_1 = []
+            for p in self.post_reader.keys():
+                if out_num_s[i] == len(self.post_reader[p]):
+                    out_1.append(p)
+                    N += 1
+            out_2 = sorted(out_1, reverse=True)
+            out = out + out_2
+            if N >= k:
+                i = len(out_num)
+        return out[:k]
